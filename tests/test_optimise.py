c8835ba90c3dcfd49d715b43a4ad8f92cbc7fcf9
Alan Heays <aheays@fastmail.com>
2020-07-03 23:10:18 +0200

    Finished optimiser?

--- tests/test_optimise.py
+++ tests/test_optimise.py
@@ -1,7 +1,11 @@
 import time
 
+from matplotlib import pyplot as plt
+
 from spectr import optimise
 from spectr import tools
+
+show_plots =False
 
 def test_ParameterSet_instantiate():
     t = optimise.ParameterSet()
@@ -90,7 +94,7 @@ def test_suboptimisers_get_parameters():
     assert len(t.get_parameters()) == 4
     u = optimise.Optimiser(z=1,w=(0.1,True,1e-5))
     t = optimise.Optimiser('t',u,x=1,y=(0.1,True,1e-5))
-    assert len(t.suboptimisers) == 1
+    assert len(t._suboptimisers) == 1
     assert len(t.get_parameters()) == 4
 
 def test_get_all_suboptimisers():
@@ -99,21 +103,21 @@ def test_get_all_suboptimisers():
     v = optimise.Optimiser(z=1,w=(0.1,True,1e-5))
     t.add_suboptimiser(u)
     u.add_suboptimiser(v)
-    assert len(t.suboptimisers) == 1
-    assert len(t.get_all_suboptimisers()) == 3
+    assert len(t._suboptimisers) == 1
+    assert len(t._get_all_suboptimisers()) == 3
     assert len(t.get_parameters()) == 6
     t.add_suboptimiser(v)
-    assert len(t.get_all_suboptimisers()) == 3
+    assert len(t._get_all_suboptimisers()) == 3
     assert len(t.get_parameters()) == 6
 
 def test_optimiser_construct():
     t = optimise.Optimiser(x=1,y=(0.1,True,1e-5))
-    t.add_construct(lambda: [1,2,3])
-    assert len(t.construct_functions) == 1
+    t.add_construct_function(lambda: [1,2,3])
+    assert len(t._construct_functions) == 1
     assert list(t.construct()) == [1,2,3]
     t = optimise.Optimiser()
     x = t.add_parameter('x',0.1,False,1e-5)
-    t.add_construct(lambda: x-1)
+    t.add_construct_function(lambda: x-1)
     assert list(t.construct()) == [-0.9]
     x.value = 0.2
     assert list(t.construct()) == [-0.8]
@@ -145,14 +149,73 @@ def test_optimiser_has_changed():
 def test_optimise():
     t = optimise.Optimiser()
     x = t.add_parameter('x',0.1, True,1e-5)
-    t.add_construct(lambda: x-1)
+    t.add_construct_function(lambda: x-1)
     residual = t.optimise()
     assert tools.rms(residual) < 1e-5
     assert abs(x.value-1) < 1e-5
 
-def test_format_input():
+def test_optimiser_format_input():
     t = optimise.Optimiser()
     assert t.format_input() == "from spectr import *\n\no = Optimiser('o')"
     t.print_input()
     t.print_input('^from')
     assert t.format_input('^from.*') == "from spectr import *\n"
+
+def test_optimiser_str():
+    t = optimise.Optimiser()
+    p = t.add_parameter_set(x=1,y=(0.1,True,1e-5))
+
+def test_plot_residual():
+    t = optimise.Optimiser('t')
+    t.add_construct_function(lambda: tools.randn(30))
+    t.add_construct_function(lambda: tools.randn(30)*2)
+    u = optimise.Optimiser('u')
+    u.add_construct_function(lambda: tools.randn(30)*3)
+    t.add_suboptimiser(u)
+    t.optimise()
+    t.plot_residual()
+    if show_plots:
+        plt.show()
+
+def test_plot():
+    t = optimise.Optimiser('t')
+    def f():
+        plt.plot([1,2,5])
+        plt.title('t')
+    t.add_plot_function(f)
+    def f():
+        plt.plot([-3,-2,-1])
+    t.add_plot_function(f)
+    u = optimise.Optimiser('u')
+    def f():
+        plt.plot([10,20,30])
+    t.add_plot_function(f)
+    t.add_suboptimiser(u)
+    t.plot()
+    if show_plots:
+        plt.show()
+
+def test_save_to_directory():
+    t = optimise.Optimiser('t')
+    u = optimise.Optimiser('u')
+    t.add_suboptimiser(u)
+    u.description = 'A description.'
+    t.add_construct_function(lambda: tools.randn(30))
+    u.add_construct_function(lambda: tools.randn(30)*3)
+    t.optimise()
+    t.save_to_directory('tmp/test_save_to_directory')
+    # def f():
+        # plt.plot([1,2,5])
+        # plt.title('t')
+    # t.add_plot_function(f)
+    # def f():
+        # plt.plot([-3,-2,-1])
+    # t.add_plot_function(f)
+    # u = optimise.Optimiser('u')
+    # def f():
+        # plt.plot([10,20,30])
+    # t.add_plot_function(f)
+    # t.add_suboptimiser(u)
+    # t.plot()
+    # if show_plots:
+        # plt.show()
