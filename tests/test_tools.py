a6e6fd288360d0d7f4d42fae91402f1968993402
Alan Heays <aheays@fastmail.com>
2020-10-07 14:50:07 +0200

    added vectorisaiton decorators

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from spectr import tools
 
 def test_import():
@@ -19,10 +21,29 @@ def test_AutoDict():
     t['y']['b'][1] = 5
     assert t['y']['b'] == {1:5}
 
-def test_vectorise_function():
-    @tools.vectorise_in_chunks
+def test_vectorise_function_in_chunks():
+    @tools.vectorise_function_in_chunks
     def f(x):
         return x**2
+    assert f(5) == 25
     assert list(f([1,2,2,3])) == [1,4,4,9]
-    
 
+def test_vectorise_function():
+    @tools.vectorise_function
+    def f(x,y):
+        if not np.isscalar(x):
+            assert not np.isscalar(y)
+        return x + y
+    assert f(5,0) == 5
+    assert list(f(1,[1,2])) == [2,3]
+    
+def test_vectorise_arguments():
+    @tools.vectorise_arguments
+    def f(x,y):
+        retval = np.full(np.shape(x),1)
+        retval[(x==2)&(y==2)] = 2
+        return retval
+    assert list(f([1,2,3],[2,2,2])) == [1,2,1]
+    assert list(f([1,2,3],2)) == [1,2,1]
+    assert f(1,2) == 1
+    
