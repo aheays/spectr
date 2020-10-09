255e9a6c40d9b709219a89f5b33107e9b1442944
Alan Heays <aheays@fastmail.com>
2020-10-09 11:10:48 +0200

    comment stuff

--- tests/test_conversions.py
+++ tests/test_conversions.py
@@ -2,24 +2,24 @@ from pprint import pprint
 import numpy as np
 from spectr import conversions
 
-def test_homonuclear_diatomic_level_degeneracy():
-    # assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=0,sa=0) == 0
-    # assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=0,sa=1) == 3
+# def test_homonuclear_diatomic_level_degeneracy():
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=0,sa=0) == 0
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=0,sa=1) == 3
 
-    ## 14N2 ground state
-    assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=1,sa=0) == 3
-    assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=1,sa=1) == 9
-    ## 15N2 ground state
-    assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=0.5,sa=0) == 3
-    assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=0.5,sa=1) == 9
+    # ## 14N2 ground state
+    # assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=1,sa=0) == 3
+    # assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=1,sa=1) == 9
+    # ## 15N2 ground state
+    # assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=0.5,sa=0) == 3
+    # assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=0.5,sa=1) == 9
 
-    # assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=0,sa=0) == 0
-    # assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=0,sa=0) == 0
-    # assert conversions.homonuclear_diatomic_level_degeneracy(1,0,1) == 3
-    # assert conversions.homonuclear_diatomic_level_degeneracy(0,1,1) == 6
-    # assert conversions.homonuclear_diatomic_level_degeneracy(1,1,1) == 18
-    # assert conversions.homonuclear_diatomic_level_degeneracy(0,0.5,1) == 1
-    # assert conversions.homonuclear_diatomic_level_degeneracy(1,0.5,1) == 3
-    # assert conversions.homonuclear_diatomic_level_degeneracy(1,1,1) == 18
-    # assert conversions.homonuclear_diatomic_level_degeneracy(0,1,1) == 2
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(J=0,Inuclear=0,sa=0) == 0
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(J=1,Inuclear=0,sa=0) == 0
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(1,0,1) == 3
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(0,1,1) == 6
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(1,1,1) == 18
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(0,0.5,1) == 1
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(1,0.5,1) == 3
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(1,1,1) == 18
+    # # assert conversions.homonuclear_diatomic_level_degeneracy(0,1,1) == 2
 
