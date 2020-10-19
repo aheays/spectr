a5eeba73c99d064b7b989efc08927fc339a82de2
Alan Heays <aheays@fastmail.com>
2020-10-19 16:06:18 +0200

    added key 'isotopologue'

--- levels.py
+++ levels.py
@@ -8,6 +8,7 @@ import numpy as np
 from .dataset import Dataset
 from .conversions import convert
 from . import tools
+from . import database
 from .exceptions import InferException
 
 ###########################################
@@ -23,18 +24,17 @@ prototypes['author'] = dict(description="Author of data or printed file" ,kind=s
 prototypes['reference'] = dict(description="Published reference" ,kind=str ,infer={})
 prototypes['date'] = dict(description="Date data collected or printed" ,kind=str ,infer={})
 prototypes['species'] = dict(description="Chemical species" ,kind=str ,infer={})
+prototypes['isotopologue'] = dict(description="Isotopologue" ,kind=str ,infer={})
 prototypes['mass'] = dict(description="Mass (amu)",kind=float, fmt='<11.4f', infer={('species',): lambda species: database.get_mass(species),})
 prototypes['reduced_mass'] = dict(description="Reduced mass (amu)", kind=float, fmt='<11.4f', infer={('species','database',): lambda species: _get_species_property(species,'reduced_mass')})
 prototypes['E'] = dict(description="Level energy (cm-1)" ,kind=float ,fmt='<14.7f' ,infer={})
 prototypes['J'] = dict(description="Total angular momentum quantum number excluding nuclear spin" , kind=float,infer={})
-
 def _f0(classname,J):
     """Calculate heteronuclear diatomic molecule level degeneracy."""
     if classname == 'DiatomicCinfv':
         return 2*J+1
     else:
         raise InferException('Only valid of DiatomicCinfv')
-
 @tools.vectorise_function
 @functools.lru_cache
 def _f1(classname,J,Inuclear,sa):
@@ -55,9 +55,7 @@ def _f1(classname,J,Inuclear,sa):
             return (2*J+1)*nodd
         else:
             return (2*J+1)*neven
-
 prototypes['g'] = dict(description="Level degeneracy including nuclear spin statistics" , kind=int , infer={('classname','J'):_f0, ('classname','J','Inuclear','sa'):_f1,})
-
 prototypes['pm'] = dict(description="Total inversion symmetry" ,kind=int ,infer={})
 prototypes['Γ'] = dict(description="Total natural linewidth of level or transition (cm-1 FWHM)" , kind=float, fmt='<10.5g', infer={('A',):lambda τ: 5.309e-12*A,})
 prototypes['τ'] = dict(description="Total decay lifetime (s)", kind=float, infer={ ('A',): lambda A: 1/A,})       
@@ -69,16 +67,13 @@ prototypes['Eref'] = dict(description="Reference point of energy scale relative 
 prototypes['Teq'] = dict(description="Equilibriated temperature (K)", kind=float, fmt='0.2f', infer={})
 prototypes['Tex'] = dict(description="Excitation temperature (K)", kind=float, fmt='0.2f', infer={'Teq':lambda Teq:Teq})
 prototypes['partition_source'] = dict(description="Data source for computing partition function, 'self' or 'database' (default).", kind=str, infer={('database',): lambda : 'database',})
-
 @tools.vectorise_function_in_chunks()
 def _f5(partition_source,species,Tex):
     from . import hitran
     if partition_source!='HITRAN':
         raise InferException(f'Partition source not "HITRAN".')
     return hitran.get_partition_function(species,Tex)
-
 prototypes['partition'] = dict(description="Partition function.", kind=float, fmt='<11.3e', infer={('partition_source','species','Tex'):_f5,})
-
 prototypes['α'] = dict(description="State population", kind=float, fmt='<11.4e', infer={('partition','E','g','Tex'): lambda partition,E,g,Tex : g*np.exp(-E/(convert(constants.Boltzmann,'J','cm-1')*Tex))/partition,})
 prototypes['Nself'] = dict(description="Column density (cm2)",kind=float,fmt='<11.3e', infer={})
 prototypes['label'] = dict(description="Label of electronic state", kind=str,infer={})
@@ -86,7 +81,6 @@ prototypes['v'] = dict(description="Vibrational quantum number", kind=int,infer=
 prototypes['Λ'] = dict(description="Total orbital angular momentum aligned with internuclear axis", kind=int,infer={})
 prototypes['LSsign'] = dict(description="For Λ>0 states this is the sign of the spin-orbit interacting energy. For Λ=0 states this is the sign of λ-B. In either case it controls whether the lowest Σ level is at the highest or lower energy.", kind=int,infer={})
 prototypes['s'] = dict(description="s=1 for Σ- states and 0 for all other states", kind=int,infer={})
-
 @tools.vectorise_arguments
 def _f0(ef,J):
     """Calculate σv symmetry"""
@@ -97,10 +91,8 @@ def _f0(ef,J):
     σv[exponent%2==1] = -1
     return σv
 prototypes['σv'] = dict(description="Symmetry with respect to σv reflection.", kind=int,infer={('ef','J'):_f0,})
-
 prototypes['gu'] = dict(description="Symmetry with respect to reflection through a plane perpendicular to the internuclear axis.", kind=int,infer={})
 prototypes['sa'] = dict(description="Symmetry with respect to nuclear exchange, s=symmetric, a=antisymmetric.", kind=int,infer={('σv','gu'):lambda σv,gu: σv*gu,})
-
 @tools.vectorise_arguments
 def _f0(S,Λ,s):
     """Calculate gu symmetry for 1Σ- and 1Σ+ states only."""
@@ -110,7 +102,6 @@ def _f0(S,Λ,s):
     ef[s==1] = -1
     return ef
 prototypes['ef'] = dict(description="e/f symmetry", kind=int,infer={('S','Λ','s'):_f0,})
-
 prototypes['Fi'] = dict(description="Spin multiplet index", kind=int,infer={})
 prototypes['Ω'] = dict(description="Absolute value of total angular momentum aligned with internuclear axis", kind=float,infer={})
 prototypes['Σ'] = dict(description="Signed spin projection in the direction of Λ, or strictly positive if Λ=0", kind=float,infer={})
@@ -144,7 +135,7 @@ class Base(_BaseLinesLevels):
 
     prototypes = copy(_BaseLinesLevels.prototypes)
     prototypes.update(**{key:copy(prototypes[key]) for key in [
-        'species','E','J','g','pm','Γ','J','N','S','Eref','Teq','Tex','partition_source','partition','α','Nself',]})
+        'species','isotopologue','E','J','g','pm','Γ','J','N','S','Eref','Teq','Tex','partition_source','partition','α','Nself',]})
         
 
 
