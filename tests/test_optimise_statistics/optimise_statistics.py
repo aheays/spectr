3f155984847af5995243a12361eb9c5cb4e6bea8
Alan Heays <aheays@fastmail.com>
2021-03-15 10:07:48 +0100

    modified script to test Optimise statistics

--- tests/test_optimise_statistics/optimise_statistics.py
+++ tests/test_optimise_statistics/optimise_statistics.py
@@ -9,8 +9,8 @@ for i in range(300):
     ## define fucnction
     x = np.linspace(0,10,1000)
     y = (
-        # 1 + 2*x
-        + 0.2*np.sin(2*x)
+        1 + 2*x
+        # + 0.2*np.sin(2*x)
         + lineshapes.gaussian(x,4,1,0.5)
         + 0.02*tools.randn(x.shape)
         # + 0.5*x**2
@@ -18,8 +18,8 @@ for i in range(300):
 
     ## optimiser to model it
     o = Optimiser(
-        # m=P(0, True,1e-3),
-        # c=P(0, True,1e-3),
+        m=P(0, True,1e-3),
+        c=P(0, True,1e-3),
         x0=P(3, True,1e-3),
         S=P(4, True,1e-3),
         w=P(2, True,1e-3),
@@ -29,29 +29,37 @@ for i in range(300):
     o.add_construct_function(
         lambda: (
             y - (
-                # float(o['c'])+float(o['m'])*x
+                float(o['c'])+float(o['m'])*x
                 + lineshapes.gaussian(x,float(o['x0']),float(o['S']),float(o['w']))
                 # +float(o['a'])*np.sin(float(o['b'])*x)
             )))
 
     ## collect statistics
+    print( o)
     o.optimise(
         monitor_frequency='never',
         verbose=False,
         # rms_noise=0.2 
     )
-    for key in o:
-        if key not in results:
-            results[key] = []
-            results['d'+key] = []
-        results[key].append(o[key].value)
-        results['d'+key].append(o[key].uncertainty)
 
-## plot statistics
-qfig(1)
-subplot()
-plot(x,y)
-plot(x,o.residual)
-for key in o:
-    subplot();plot_hist_with_fitted_gaussian(results[key]);title(key)
-    subplot();plot_hist_with_fitted_gaussian(results['d'+key]);title('d'+key)
+    print( o)
+
+    o.calculate_uncertainty()
+    print( o)
+    break
+    
+    # for key in o:
+        # if key not in results:
+            # results[key] = []
+            # results['d'+key] = []
+        # results[key].append(o[key].value)
+        # results['d'+key].append(o[key].uncertainty)
+
+# ## plot statistics
+# qfig(1)
+# subplot()
+# plot(x,y)
+# plot(x,o.residual)
+# for key in o:
+    # subplot();plot_hist_with_fitted_gaussian(results[key]);title(key)
+    # subplot();plot_hist_with_fitted_gaussian(results['d'+key]);title('d'+key)
