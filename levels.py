fixed units in prototypes

levels.py
@@ -52,8 +52,8 @@ def _f1(self,species):
         return database.get_species_property(species,'mass')
     except MissingDataException as err:
         raise InferException(str(err))
-prototypes['mass'] = dict(description="Mass (amu)",kind='f', fmt='<11.4f', infer=[(('species',), _f0),])
-prototypes['reduced_mass'] = dict(description="Reduced mass (amu)", kind='f', fmt='<11.4f', infer=[(('species','database',), lambda self,species: _get_species_property(species,'reduced_mass'))])
+prototypes['mass'] = dict(description="Mass",units="amu",kind='f', fmt='<11.4f', infer=[(('species',), _f0),])
+prototypes['reduced_mass'] = dict(description="Reduced mass",units="amu", kind='f', fmt='<11.4f', infer=[(('species','database',), lambda self,species: _get_species_property(species,'reduced_mass'))])
 
 @vectorise(cache=True)
 def _f0(self,species,label,v,Σ,ef,J):
@@ -73,7 +73,7 @@ prototypes['Ee'] = dict(description="Level energy relative to equilibrium geomet
 prototypes['Eresidual'] = dict(description="Residual error of level energy" ,units='cm-1',kind='f' ,fmt='<14.7f' ,infer=[],default_step=1e-3)
 prototypes['ZPE'] = dict(description="Zero-point energy of the lowest level relative to Ee" ,units='cm-1',kind='f' ,fmt='<14.7f' ,infer=[('species',lambda self,species: database.get_species_property(species,'ZPE')),],default_step=1e-3)
 prototypes['J'] = dict(description="Total angular momentum quantum number excluding nuclear spin" , kind='f',infer=[])
-prototypes['ΓD'] = dict(description="Gaussian Doppler width (cm-1 FWHM)",kind='f',fmt='<10.5g', infer=[(('mass','Ttr','ν'), lambda self,mass,Ttr,ν:2.*6.331e-8*np.sqrt(Ttr*32./mass)*ν)])
+prototypes['ΓD'] = dict(description="Gaussian Doppler width",units="cm-1 FWHM",kind='f',fmt='<10.5g', infer=[(('mass','Ttr','ν'), lambda self,mass,Ttr,ν:2.*6.331e-8*np.sqrt(Ttr*32./mass)*ν)])
 
 def _f0(self,species,label,v,Σ,ef,J,E):
     """Compute separate best-fit reduced energy levels for each
@@ -146,20 +146,19 @@ def _f1(self,point_group,Inuclear,sa):
     else:
         raise InferException()
 
-prototypes['conf'] = dict(description="Electronic configuration.", kind='U', fmt='10s', infer=[])
-prototypes['gnuclear'] = dict(description="Nuclear spin level degeneracy (relative only)." , kind='i' , infer=[(('point_group',),_f0),( ('point_group','Inuclear','sa'),_f1),])
+prototypes['conf'] = dict(description="Electronic configuration", kind='U', fmt='10s', infer=[])
+prototypes['gnuclear'] = dict(description="Nuclear spin level degeneracy (relative only)" , kind='i' , infer=[(('point_group',),_f0),( ('point_group','Inuclear','sa'),_f1),])
 prototypes['g'] = dict(description="Level degeneracy including nuclear spin statistics" , kind='i' , infer=[(('J','gnuclear'),lambda self,J,gnuclear: (2*J+1)*gnuclear,)])
 # prototypes['pm'] = dict(description="Total inversion symmetry" ,kind='i' ,infer=[])
-prototypes['Γ'] = dict(description="Total natural linewidth of level or transition (cm-1 FWHM)" , kind='f', fmt='<10.5g', infer=[(('A',),lambda self,τ: 5.309e-12*A,)])
-prototypes['τ'] = dict(description="Total decay lifetime (s)", kind='f', infer=[(('A',), lambda self,A: 1/A,)])       
-prototypes['A'] = dict(description="Total decay rate (s-1)", kind='f', infer=[(('Γ',),lambda self,Γ: Γ/5.309e-12,)])
+prototypes['Γ'] = dict(description="Total natural linewidth of level or transition" ,units="cm-1 FWHM",kind='f',cast=cast_abs_float_array,fmt='<10.5g', infer=[(('A',),lambda self,τ: 5.309e-12*A,)])
+prototypes['τ'] = dict(description="Total decay lifetime",units="s", kind='f', infer=[(('A',), lambda self,A: 1/A,)])       
+prototypes['A'] = dict(description="Total decay rate",units="s-1", kind='f', infer=[(('Γ',),lambda self,Γ: Γ/5.309e-12,)])
 prototypes['J'] = dict(description="Total angular momentum quantum number excluding nuclear spin", kind='f',fmt='>0.1f',infer=[])
 prototypes['N'] = dict(description="Angular momentum excluding nuclear and electronic spin", kind='f', infer=[(('J','SR'),lambda self,J,SR: J-SR,)])
 prototypes['S'] = dict(description="Total electronic spin quantum number", kind='f',infer=[(('chemical_species','label'),lambda self,chemical_species,label: database.get_electronic_state_property(chemical_species,label,'S'),)])
 # prototypes['Eref'] = dict(description="Reference point of energy scale relative to potential-energy minimum.",units='cm-1', kind='f',infer=[((),lambda self,: 0.,)])
-prototypes['Teq'] = dict(description="Equilibriated temperature (K)", kind='f', fmt='0.2f', infer=[],cast=cast_abs_float_array)
-prototypes['Tex'] = dict(description="Excitation temperature (K)", kind='f', fmt='0.2f', infer=[('Teq',lambda self,Teq:Teq)],cast=cast_abs_float_array)
-# prototypes['partition_source'] = dict(description="Data source for computing partition function, 'self' or 'database' (default).", kind='U', infer=[(('database',), lambda self,: 'database',)])
+prototypes['Teq'] = dict(description="Equilibriated temperature",units="K", kind='f', fmt='0.2f', infer=[],cast=cast_abs_float_array)
+prototypes['Tex'] = dict(description="Excitation temperature",units="K", kind='f', fmt='0.2f', infer=[('Teq',lambda self,Teq:Teq)],cast=cast_abs_float_array)
 
 # @vectorise(cache=True,vargs=(1,2))
 def _f5(self,species,Tex):
@@ -181,7 +180,7 @@ prototypes['Z'] = dict(description="Partition function.", kind='f', fmt='<11.3e'
     (('species','Tex'),_f5),
 ])
 prototypes['α'] = dict(description="State population", kind='f', fmt='<11.4e', infer=[(('Z','E','g','Tex'), lambda self,Z,E,g,Tex : g*np.exp(-E/(convert.units(constants.Boltzmann,'J','cm-1')*Tex))/Z,)])
-prototypes['Nself'] = dict(description="Column density (cm2)",kind='f',fmt='<11.3e', infer=[])
+prototypes['Nself'] = dict(description="Column density",units="cm2",kind='f',fmt='<11.3e', infer=[])
 prototypes['label'] = dict(description="Label of electronic state", kind='U',infer=[])
 prototypes['v'] = dict(description="Vibrational quantum number", kind='i',infer=[])
 prototypes['ν1'] = dict(description="Vibrational quantum number for mode 1", kind='i',infer=[])
@@ -298,7 +297,7 @@ def _f6(self,Λ,S,Σ,s,ef,LSsign):
 def _f5(self,S):
     if not np.all(S==0): raise InferException()
     return(np.zeros(S.shape))
-prototypes['SR'] = dict(description="Signed projection of spin angular momentum onto the molecule-fixed z (rotation) axis.", kind='f',infer=[
+prototypes['SR'] = dict(description="Signed projection of spin angular momentum onto the molecule-fixed z axis", kind='f',infer=[
     (('S',), _f5), # trivial case, S=0
     (('J','N'),lambda self,J,N: J-N),
     (('S','F'),lambda self,S,F: S-F+1),                   # Fi ordering follows decreasing SR
@@ -322,64 +321,35 @@ prototypes['Inuclear'] = dict(description="Nuclear spin of individual nuclei.", 
 
 ## Effective Hamiltonian parameters
 prototypes['Tv']  = dict(description='Term origin' ,units='cm-1',kind='f',fmt='0.6f',infer=[])
-# prototypes['dTv'] = dict(description='Uncertainty in Term origin (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['Bv']  = dict(description='Rotational constant' ,units='cm-1',kind='f',fmt='0.8f',infer=[])
-# prototypes['dBv'] = dict(description='Uncertainty in rotational constant (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['Dv']  = dict(description='Centrifugal distortion' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dDv'] = dict(description='Uncertainty in centrifugal distortion (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['Hv']  = dict(description='Third order centrifugal distortion' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dHv'] = dict(description='Uncertainty in thrid order centrifugal distortion (1σ, cm-1)' ,kind='f',fmt='3g',infer=[])
 prototypes['Lv']  = dict(description='Fourth order centrifugal distortion' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dLv'] = dict(description='Uncertainty in fourth order centrifugal distortion (1σ, cm-1)' ,kind='f',fmt='3g',infer=[])
 prototypes['Av']  = dict(description='Spin-orbit energy' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dAv'] = dict(description='Uncertainty in spin-orbit energy (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['ADv'] = dict(description='Spin-orbit centrifugal distortion',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dADv']= dict(description='Uncertainty in spin-orbit centrifugal distortion (1σ, cm-1)',kind='f',fmt='0.2g',infer=[])
 prototypes['AHv'] = dict(description='Higher-order spin-orbit centrifugal distortion',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dAHv']= dict(description='Uncertainty in higher-order spin-orbit centrifugal distortion (1σ, cm-1)',kind='f',fmt='0.2g',infer=[])
 prototypes['λv']  = dict(description='Spin-spin energy',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dλv'] = dict(description='Uncertainty in spin-spin energy (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['λDv'] = dict(description='Spin-spin centrifugal distortion',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dλDv']= dict(description='Uncertainty in spin-spin centrifugal distortion (1σ, cm-1)',kind='f',fmt='0.2g',infer=[])
 prototypes['λHv'] = dict(description='Higher-order spin-spin centrifugal distortion',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dλHv']= dict(description='Uncertainty in higher-order spin-spin centrifugal distortion (1σ, cm-1)',kind='f',fmt='0.2g',infer=[])
 prototypes['γv']  = dict(description='Spin-rotation energy' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dγv'] = dict(description='Uncertainty in spin-rotation energy (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['γDv'] = dict(description='Spin-rotation centrifugal distortion',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dγDv']= dict(description='Uncertainty in spin-rotation centrifugal distortion',units='cm-1',kind='f',fmt='0.2g',infer=[])
 prototypes['γHv'] = dict(description='Higher-orders pin-rotation centrifugal distortion',units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dγHv']= dict(description='Uncertainty in higher-order spin-rotation centrifugal distortion',units='cm-1',kind='f',fmt='0.2g',infer=[])
 prototypes['ov']  = dict(description='Λ-doubling constant o' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dov'] = dict(description='Uncertainty in Λ-doubling constant o (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['oDv']  = dict(description='Higher-order Λ-doubling constant o' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['doDv'] = dict(description='Uncertainty in higher-order Λ-doubling constant o (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['oHv']  = dict(description='Higher-order Λ-doubling constant o' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['doHv'] = dict(description='Uncertainty in higher-order Λ-doubling constant o (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['oLv']  = dict(description='Ligher-order Λ-doubling constant o' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['doLv'] = dict(description='Uncertainty in higher-order Λ-doubling constant o (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['pv']  = dict(description='Λ-doubling constant p' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dpv'] = dict(description='Uncertainty in Λ-doubling constant p (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['pDv']  = dict(description='Higher-order Λ-doubling constant p' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dpDv'] = dict(description='Uncertainty in higher-order Λ-doubling constant p (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['qv']  = dict(description='Λ-doubling constant q' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dqv'] = dict(description='Uncertainty in Λ-doubling constant q (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 prototypes['qDv']  = dict(description='Higher-order Λ-doubling constant q' ,units='cm-1',kind='f',fmt='0.6g',infer=[])
-# prototypes['dqDv'] = dict(description='Uncertainty in higher-order Λ-doubling constant q (1σ, cm-1)' ,kind='f',fmt='0.2g',infer=[])
 
-prototypes['Γv'] = dict(description="Total electronic-vibrational linewidth (cm-1 FWHM)", kind='f',  fmt='<10.5g', strictly_positive=True, infer=[(('τ',),lambda self,τ: 5.309e-12/τ,)]) # tau=1/2/pi/gamma/c
-# prototypes['dΓv'] = dict(description="Uncertainty in total electronic-vibrational linewidth (cm-1 FWHM 1σ)", kind='f',  fmt='<10.5g', infer=[(('Γ','τ','dτ'), lambda self,Γ,τ,dτ: dτ*Γ/τ,)])
-prototypes['τv'] = dict(description="Total electronic-vibrational decay lifetime (s)", kind='f',  fmt='<10.5g', infer=[(('Γv',), lambda self,Γv: 5.309e-12/Γv),( ('Atv',), lambda self,Atv: 1/Atv,)]) 
-# prototypes['dτv'] = dict(description="Uncertainty in total electronic-vibrational decay lifetime (s 1σ)", kind='f',  fmt='<10.5g', infer=[(('Γ','dΓ','τ'), lambda self,Γ,dΓ,τ: dΓ/Γ*τ),(('At','dAt','τ'), lambda self,At,dAt,τ: dAt/At*τ,)])
-prototypes['Atv'] = dict(description="Total electronic-vibrational decay rate (s-1)", kind='f',  fmt='<10.5g', infer=[(('τv',), lambda self,τv: 1/τv), (('Adv','Ave'), lambda self,Adv,Aev: Adv+Aev,), (('Aev',), lambda self,Aev: Aev),( ('Adv',), lambda self,Adv: Adv,)])# Test for Ad and Ae, if failed then one or the other is undefined/zero
-# prototypes['dAtv']= dict(description="Uncertainty in total electronic-vibrational decay rate (s-1 1σ)", kind='f',  fmt='<10.5g', infer=[(('τ','dτ','At'), lambda self,τ,dτ,At: dτ/τ*At,)])
-prototypes['Adv'] = dict(description="Nonradiative electronic-vibrational decay rate (s-1)", kind='f',  fmt='<10.5g', infer=[(('At','Ae'), lambda self,At,Ae: At-Ae,)])
-# prototypes['dAdv']= dict(description="Uncertainty in nonradiative electronic-vibrational decay rate (s-1 1σ)", kind='f',  fmt='<10.5g', infer=[])
-prototypes['Aev'] = dict(description="Radiative electronic-vibrational decay rate (s-1)", kind='f',  fmt='<10.5g', infer=[(('At','Ad'), lambda self,At,Ad: At-Ad,)])
-# prototypes['dAev']= dict(description="Uncertainty in radiative electronic-vibrational decay rate (s-1 1σ)", kind='f',  fmt='<10.5g', infer=[])
-prototypes['ηdv'] = dict(description="Fractional probability of electronic-vibrational level decaying nonradiatively (dimensionless)", kind='f',  fmt='<10.5g', infer=[(('At','Ad'),lambda self,At,Ad:Ad/A,)])
-# prototypes['dηdv']= dict(description="Uncertainty in the fractional probability of electronic-vibrational level decaying by dissociation by any channels (dimensionless, 1σ)", kind='f',  fmt='<10.5g', infer=[])
-prototypes['ηev'] = dict(description="Fractional probability of electronic-vibrational level decaying radiatively (dimensionless)", kind='f',  fmt='<10.5g', infer=[(('At','Ae'),lambda self,At,Ae:Ae/A,)])
-# prototypes['dηev']= dict(description="Uncertainty in the fractional probability of electronic-vibrational level decaying radiatively (dimensionless, 1σ)", kind='f',  fmt='<10.5g', infer=[])
+prototypes['Γv'] = dict(description="Total electronic-vibrational linewidth",units="cm-1 FWHM", kind='f',  fmt='<10.5g', strictly_positive=True, infer=[(('τ',),lambda self,τ: 5.309e-12/τ,)]) # tau=1/2/pi/gamma/c
+prototypes['τv'] = dict(description="Total electronic-vibrational decay lifetime",units="s", kind='f',  fmt='<10.5g', infer=[(('Γv',), lambda self,Γv: 5.309e-12/Γv),( ('Atv',), lambda self,Atv: 1/Atv,)]) 
+prototypes['Atv'] = dict(description="Total electronic-vibrational decay rate",units="s-1", kind='f',  fmt='<10.5g', infer=[(('τv',), lambda self,τv: 1/τv), (('Adv','Ave'), lambda self,Adv,Aev: Adv+Aev,), (('Aev',), lambda self,Aev: Aev),( ('Adv',), lambda self,Adv: Adv,)])# Test for Ad and Ae, if failed then one or the other is undefined/zero
+prototypes['Adv'] = dict(description="Nonradiative electronic-vibrational decay rate",units="s-1", kind='f',  fmt='<10.5g', infer=[(('At','Ae'), lambda self,At,Ae: At-Ae,)])
+prototypes['Aev'] = dict(description="Radiative electronic-vibrational decay rate",units="s-1", kind='f',  fmt='<10.5g', infer=[(('At','Ad'), lambda self,At,Ad: At-Ad,)])
+prototypes['ηdv'] = dict(description="Fractional probability of electronic-vibrational level decaying nonradiatively",units=None, kind='f',  fmt='<10.5g', infer=[(('At','Ad'),lambda self,At,Ad:Ad/A,)])
+prototypes['ηev'] = dict(description="Fractional probability of electronic-vibrational level decaying radiatively",units=None, kind='f',  fmt='<10.5g', infer=[(('At','Ae'),lambda self,At,Ae:Ae/A,)])
 
 ## v+1 reduced versions of these
 def _vibrationally_reduce(
@@ -403,15 +373,12 @@ def _vibrationally_reduce(
         p = my.polyfit(x,y,dy,order=max(0,min(reduced_polynomial_order,sum(i)-2)),error_on_missing_dy=False)
         reduced[i] = p['residuals']
     return(reduced)
-prototypes['Tvreduced'] = dict(description="Vibrational term value reduced by a polynomial in (v+1/2)",units='cm-1', kind='f',  fmt='<11.4f',
+prototypes['Tvreduced'] = dict(description="Vibrational term value reduced by a polynomial in v+1/2",units='cm-1', kind='f',  fmt='<11.4f',
     infer=[(('self','reduced_quantum_number','reduced_polynomial_order','Tv','dTv'), _vibrationally_reduce), # dTv is known -- use in a weighted mean
            (('self','reduced_quantum_number','reduced_polynomial_order','Tv'), _vibrationally_reduce,)]) # dTv is not known
-prototypes['dTvreduced'] = dict(description="Uncertainty in vibrational term value reduced by a polynomial in (v+1/2) (cm-1 1σ)", kind='f',  fmt='<11.4f', infer=[(('dT',),lambda self,dT: dT,)])
-prototypes['Tvreduced_common'] = dict(description="Term values reduced by a common polynomial in (v+1/2)",units='cm-1', kind='f',  fmt='<11.4f', infer=[(('v','Tv','Tvreduced_common_polynomial'), lambda self,v,Tv,Tvreduced_common_polynomial: Tv-np.polyval(Tvreduced_common_polynomial,v+0.5)),(('v','Tv'), lambda self,v,Tv: Tv-np.polyval(np.polyfit(v+0.5,Tv,3),v+0.5),)])
-prototypes['dTvreduced_common'] = dict(description="Uncertaintty in term values reduced by a common polynomial in (v+1/2) (cm-1 1σ)", kind='f',  fmt='<11.4e', infer=[(('dTv',), lambda self,dTv: dTv)])
-prototypes['Tvreduced_common_polynomial'] = dict(description="Polynomial in terms of (v+1/2) to reduce all term values commonly",units='cm-1', kind='o', infer=[])
+prototypes['Tvreduced_common'] = dict(description="Term values reduced by a common polynomial in v+1/2",units='cm-1', kind='f',  fmt='<11.4f', infer=[(('v','Tv','Tvreduced_common_polynomial'), lambda self,v,Tv,Tvreduced_common_polynomial: Tv-np.polyval(Tvreduced_common_polynomial,v+0.5)),(('v','Tv'), lambda self,v,Tv: Tv-np.polyval(np.polyfit(v+0.5,Tv,3),v+0.5),)])
+prototypes['Tvreduced_common_polynomial'] = dict(description="Polynomial in terms of v+1/2 to reduce all term values commonly",units='cm-1', kind='o', infer=[])
 prototypes['Bv_μscaled']  = dict(description='Rotational constant scaled by reduced mass to an isotopologue-independent value' ,units='cm-1', kind='f',fmt='0.8f', infer=[(('Bv','reduced_mass'),lambda self,Bv,reduced_mass: Bv*reduced_mass,)])
-prototypes['dBv_μscaled'] = dict(description='Uncertainty in Bv_μscaled (1σ, cm-1)' ,kind='f',fmt='0.2g', infer=[(('Bv','dBv','Bv_μscaled'),lambda self,Bv,dBv,Bv_μscaled:dBv/Bv*Bv_μscaled,)])
 
 def _collect_prototypes(*keys):
     retval = {key:prototypes[key] for key in keys}