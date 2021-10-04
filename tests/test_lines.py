97b6f8b62a1d639faaef1f00a0133e48b063f210
Alan Heays <aheays@fastmail.com>
2021-10-04 15:46:00 +0200

    tests

--- tests/test_lines.py
+++ tests/test_lines.py
@@ -6,8 +6,9 @@ from spectr import levels
 from spectr import plotting
 from spectr import hitran
 from spectr import convert
+from spectr import tools
 
-show_plots =False
+show_plots = True
 
 def test_construct():
     t = lines.Generic()
@@ -27,13 +28,13 @@ def test_infer_with_level_keys():
     assert t['E_l'] == [100.]
     assert t['E_u'] == [150.]
     assert t['ν'] == [50.]
-    t = lines.Generic(ν=[100],E_u=[150])
+    t = lines.Generic(data={'ν':[100],'E_u':[150]})
     assert t['E_l'] == [50.]
-    t = lines.Generic(E_l=[100],unc_E_l=[0.5],E_u=[150],unc_E_u=[0.2])
-    t.set('E_l',[0.5],'unc')
-    t.set('E_u',[0.2],'unc')
+    t = lines.Generic(data={'E_l':[100],'E_l:unc':[0.5],'E_u':[150],'E_u:unc':[0.2]})
+    t.set('E_l','unc',0.5)
+    t.set('E_u','unc',0.2)
     assert t['ν'] == [50.]
-    assert t.get('ν','unc') == [approx(np.sqrt(0.5**2+0.2**2))]
+    assert t['ν','unc'] == [approx(np.sqrt(0.5**2+0.2**2))]
 
 def test_load_lines():
     t = lines.Generic()
@@ -49,6 +50,7 @@ def test_calculate_plot_spectrum():
     assert sum(y) == approx(0.00903753325337632)
     t.plot_spectrum(xkey='ν',ykey='f')
     if show_plots:
+        plotting.plt.title('test_calculate_plot_spectrum')
         plotting.show()
 
 def test_get_upper_lower_levels():
@@ -61,33 +63,31 @@ def test_get_upper_lower_levels():
 
 def test_spectrum_calc_against_hapi():
     νbeg,νend = 2127,2141
+    Teq = 296
     ## calc with hapi
     xhapi,yhapi = hitran.calc_spectrum(
         species='CO',
         data_directory='data/hitran_data/CO/[12C][16O]/',
-        T=296,p=1,
+        T=Teq,p=1,
         νbeg=νbeg,νend=νend,νstep=0.001,
         table_name='hitran_linelist',
-        make_plot=False,
-    )
+        make_plot=False,)
     ## calc with spectr
-    l = lines.Generic()
-    l.load(f'data/hitran_data/CO/[12C][16O]/lines.h5')
-    l['Zsource'] = 'HITRAN'
-    l['Teq'] = 296
+    l = hitran.get_lines('¹²C¹⁶O')
+    l['Teq'] = Teq
     l['pair'] = convert.units(1,'atm','Pa')
-    l.limit_to_match(ν_min=νbeg,ν_max=νend)
-    xspectr,yspectr = l.calculate_spectrum(np.arange(νbeg,νend,0.001),ykey='σ')
+    l.limit_to_match(min_ν=νbeg,max_ν=νend)
+    xspectr,yspectr = l.calculate_spectrum(np.arange(νbeg,νend,0.001),ykey='S')
     from scipy import integrate
-    
-    inthapi = integrate.trapz(yhapi,xhapi)
-    intspectr = integrate.trapz(yspectr,xspectr)
+    inthapi = tools.integrate(xhapi,yhapi)
+    intspectr = tools.integrate(xspectr,yspectr)
     if show_plots:
         ax = plotting.qax()
-        ax.plot(xhapi,yhapi,label='hapi')
-        ax.plot(xspectr,yspectr,label='spectr')
-        ax.plot(xspectr,np.abs(yhapi[:-1]-yspectr),label='hapi-spectr')
+        ax.plot(xhapi,yhapi,label=f'hapi: {inthapi:0.3e}')
+        ax.plot(xspectr,yspectr,label=f'spectr:  {intspectr:0.3e}')
+        ax.plot(xspectr,np.abs(yhapi[:-1]-yspectr),label=f'hapi-spectr: {(intspectr-inthapi)/inthapi:0.3e}')
         ax.legend()
+        plotting.plt.title('test_spectrum_calc_against_hapi')
         plotting.show()
     ## not a very stringent threshold!
     assert np.all(np.abs(yhapi[:-1]-yspectr) < 3e-20)
