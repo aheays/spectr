98652c6d89aed982658d195f48b7cd62645aa2fd
Alan Heays <aheays@fastmail.com>
2022-01-25 16:04:21 +0000

    added more imports

--- env.py
+++ env.py
@@ -11,14 +11,40 @@ from numpy import array,nan,arange,linspace,logspace,isnan,inf
 from scipy import integrate
 from scipy.constants import pi as π
 from scipy.constants import Boltzmann as kB
+from matplotlib.pyplot import * 
 
-## import subpackages of this library
+## import top level
+import spectr
+
+## try to import fortran stuff
 try:
     from .fortran_tools import fortran_tools
 except ModuleNotFoundError:
     fortran_tools = None
     warnings.warn("Could not import fortran_tools.  Is it compiled?")
 
+
+## import submodules referenced to top level
+import spectr.tools
+import spectr.quantum_numbers
+import spectr.dataset
+import spectr.optimise
+import spectr.plotting
+import spectr.database
+import spectr.levels
+import spectr.lines
+import spectr.spectrum
+import spectr.kinetics
+import spectr.thermochemistry
+import spectr.atmosphere
+import spectr.hitran
+import spectr.bruker
+import spectr.electronic_states
+import spectr.viblevel
+import spectr.lineshapes
+import spectr.convert
+
+## import submodules referenced to nothing
 from . import tools
 from . import quantum_numbers
 from . import dataset
@@ -38,10 +64,10 @@ from . import viblevel
 from . import lineshapes
 from . import convert
 
+## import contents of submodules
 from .tools import *
 from .dataset import Dataset
 from .convert import units
-from matplotlib.pyplot import * # before plotting
 from .optimise import Optimiser,Parameter,P,Fixed
 from .plotting import *
-from . import plotting
+
